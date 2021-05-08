b6583505de05b6f6dcbac917bb9efaf9fe265054
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2021-05-08 12:06:48 -0300

    Adding Header vue component

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app-bar
+    app
+    color="blue accent-4"
+    dark
+  >
+    <v-toolbar-title>Movie List Aggregator</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn
+      href="https://github.com/doougui/quikdev"
+      target="_blank"
+      text
+    >
+      <span class="mr-2">See on GitHub</span>
+      <v-icon>mdi-open-in-new</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
