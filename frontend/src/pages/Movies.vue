d945de661edb5bdc5ccf60db8e14ea6a330b7e7a
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2021-05-08 16:57:17 -0300

    Fetching movies by genres

--- frontend/src/pages/Movies.vue
+++ frontend/src/pages/Movies.vue
@@ -5,7 +5,7 @@
     </header>
     <v-main>
       <v-container>
-        <h1 class="mt-3">Trending Movies</h1>
+        <h1 class="mt-3">{{ search ? `Results for "${search}"` : 'Trending Movies' }}</h1>
 
         <v-text-field
           label="Search by movie name"
@@ -16,14 +16,16 @@
 
         <v-select
           :items="genres"
+          item-value="id"
           item-text="name"
-          :value="genres.id"
           :menu-props="{ maxHeight: '400' }"
           label="Genres"
           multiple
           hint="Pick your desired genres"
           class="my-5"
           persistent-hint
+          v-model="selectedGenres"
+          @change="fetchMovies"
         ></v-select>
 
         <MovieList :movies="sortedMovies" />
@@ -48,6 +50,7 @@ export default {
   data: () => ({
     search: '',
     movies: [],
+    selectedGenres: [],
     genres: [],
   }),
 
@@ -63,7 +66,7 @@ export default {
       return [...this.movies].sort((a, b) => {
         return (a.original_title > b.original_title) ? 1 : -1;
       });
-    }
+    },
   },
 
   methods: {
@@ -73,8 +76,15 @@ export default {
           .then(response => this.movies = response.data.results);
       }
 
-      axios.get(`http://localhost:8000/movies?name=${this.search}`)
-        .then(response => this.movies = response.data.results);
+      if (this.search) {
+        axios.get(`http://localhost:8000/movies?name=${this.search}`)
+          .then(response => this.movies = response.data.results);
+      }
+
+      if (this.selectedGenres.length) {
+        axios.get(`http://localhost:8000/movies?genres=${this.selectedGenres.join(',')}`)
+          .then(response => this.movies = response.data.results);
+      }
     }
   }
 };
